da852c0e6521238bb08d84a7dcf7d5b7d9ebc32e
Torreip <80983183+Torreip@users.noreply.github.com>
2024-09-15 15:54:07 +0200

    style: db-setup - 05 (FINISHED)
    - Formating file "prisma.ts" to adhere to the correct conventions

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,7 +1,6 @@
 // ts-ignore 7017 is used to ignore the error that the global object is not
 // defined in the global scope. This is because the global object is only
 // defined in the global scope in Node.js and not in the browser.
-
 import { PrismaClient } from "@prisma/client";
 
 // PrismaClient is attached to the `global` object in development to prevent
@@ -15,5 +14,4 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient };
 export const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-
 export default prisma;
